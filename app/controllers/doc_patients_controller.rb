43f2b6c2283ac0ff4eb9f4a3dc9252fe45338007
Blake Dempsey <bdempsey864@gmail.com>
2021-09-27 11:51:42 -0600

    Final commit

--- app/controllers/doc_patients_controller.rb
+++ app/controllers/doc_patients_controller.rb
@@ -1,6 +1,6 @@
 class DocPatientsController < ApplicationController
   def destroy
-    DocPatient.where(patient_id: params[:id])
+    DocPatient.where(patient_id: params[:id]).destroy_all
     redirect_to doctor_path
   end
 end
